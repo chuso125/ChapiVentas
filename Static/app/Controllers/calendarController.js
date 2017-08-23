2a78a500fab9e1c0a8d98198bfddbe77fb6723de
Sergio Gomez <chuzo1225@hotmail.com>
2017-08-22 21:10:38 -0600

    Calendario

--- Static/app/Controllers/calendarController.js
+++ Static/app/Controllers/calendarController.js
@@ -7,10 +7,12 @@
 
     calendarController.$inject = [
                                 'calendarConfig',
+                                'productsRepository',
+                                'importantDatesRepository',
                                 "$scope", 
                                 "$rootScope"
                                 ];
-    function calendarController( calendarConfig, $scope, $rootScope) {
+    function calendarController( calendarConfig, productsRepository, importantDatesRepository, $scope, $rootScope) {
         var vm = this;
            vm.calendarView = 'month';
         vm.viewDate = new Date();
@@ -26,33 +28,53 @@
                 }
             }];
         vm.events = [
-        {
-            title: 'An event',
-            color: calendarConfig.colorTypes.warning,
-            startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
-            endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
-            draggable: true,
-            resizable: true,
-            actions: actions
-        }, {
-            title: '<i class="glyphicon glyphicon-asterisk"></i> <span class="text-primary">Another event</span>, with a <i>html</i> title',
-            color: calendarConfig.colorTypes.info,
-            startsAt: moment().subtract(1, 'day').toDate(),
-            endsAt: moment().add(5, 'days').toDate(),
-            draggable: true,
-            resizable: true,
-            actions: actions
-        }, {
-            title: 'This is a really long event title that occurs on every year',
-            color: calendarConfig.colorTypes.important,
-            startsAt: moment().startOf('day').add(7, 'hours').toDate(),
-            endsAt: moment().startOf('day').add(19, 'hours').toDate(),
-            recursOn: 'year',
-            draggable: true,
-            resizable: true,
-            actions: actions
-        }
+            {
+                title: 'An event',
+                color: calendarConfig.colorTypes.warning,
+                startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
+                endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
+                draggable: true,
+                resizable: true,
+                actions: actions,
+                editable: true
+            }, {
+                title: '<i class="glyphicon glyphicon-asterisk"></i> <span class="text-primary">Another event</span>, with a <i>html</i> title',
+                color: calendarConfig.colorTypes.info,
+                startsAt: moment().subtract(1, 'day').toDate(),
+                endsAt: moment().add(5, 'days').toDate(),
+                draggable: true,
+                resizable: true,
+                actions: actions,
+                editable: true
+            }, {
+                title: 'This is a really long event title that occurs on every year',
+                color: calendarConfig.colorTypes.important,
+                startsAt: moment().startOf('day').add(7, 'hours').toDate(),
+                endsAt: moment().startOf('day').add(19, 'hours').toDate(),
+                recursOn: 'year',
+                draggable: true,
+                resizable: true,
+                actions: actions,
+                editable: true
+            }
         ];
+
+        vm.products = productsRepository.obtainAvailableProducts();
+
+        importantDatesRepository.obtainImportantDates().forEach(function(element) {
+            vm.events.push({
+                title: element.title,
+                startsAt: element.startsAt,
+                endsAt: element.endsAt,
+                color: {
+                    primary: "#ebdb23",
+                    secondary: "#ebdb23"
+                },
+                draggable: true,
+                resizable: true,
+                editable: false
+            })
+        }, this);
 
         vm.cellIsOpen = true;
 
