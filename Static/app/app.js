686c6c60285e380188562ba31bf3cdf3941fb8d8
Sergio Gomez <chuzo1225@hotmail.com>
2017-08-22 19:51:07 -0600

    Initial commit

---
+++ Static/app/app.js
@@ -0,0 +1,9 @@
+angular.module("app",[
+    'ngSanitize',
+    'ui.router',
+    'ui.grid',
+    'mwl.calendar',
+    'ui.bootstrap',
+    'ngAnimate',
+    'colorpicker.module'
+]);
---
+++ Static/app/config.js
@@ -0,0 +1,46 @@
+(function() {
+    "use strict";
+
+    angular
+        .module("app")
+        .config(config);
+
+    config.$inject = [
+        '$stateProvider',
+        '$locationProvider',
+        '$urlRouterProvider'
+    ];
+
+    function config($stateProvider, $locationProvider, $urlRouterProvider) {
+        var home = {
+            name: 'home',
+            url: '/',
+            templateUrl: '/Static/Views/home.html',
+            controller: 'homeController',
+            controllerAs: 'vmHome'
+        };
+
+        var report = {
+            name: 'report',
+            url: '/report',
+            templateUrl: '/Static/Views/report.html',
+            controller: 'reportsController',
+            controllerAs: 'vmReport'
+        };
+
+        var calendar = {
+            name: 'calendar',
+            url: '/calendar',
+            templateUrl: '/Static/Views/calendar.html',
+            controller: 'calendarController',
+            controllerAs: 'calendarCtrl'
+        };
+
+        $stateProvider.state(home);
+        $stateProvider.state(report);
+        $stateProvider.state(calendar);
+
+        $urlRouterProvider.otherwise('/');
+        //$locationProvider.html5Mode(true);
+    }
+})();
